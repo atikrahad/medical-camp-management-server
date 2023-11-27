fb0d8f7455673eb2afd85670fa1f5d5e1a1aafa8
unknown <atikrahad1@gmail.com>
2023-11-27 15:52:50 +0600

    create a server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res)=> {
+    console.log(res.send('hellow dere'));
+})
+
+app.listen(port, ()=>{
+
+})
