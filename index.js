9d59ba24bf3a95f2a2a28cb83f4bd17b852c9721
unknown <atikrahad1@gmail.com>
2023-11-28 10:15:02 +0600

    create post api for user info

--- index.js
+++ index.js
@@ -24,14 +24,22 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+    
+    const userCollecton = client.db('medicalcampDB').collection('users')
+
+    app.post("/user", async(req, res)=>{
+      const user = req.body;
+      const result = await userCollecton.insertOne(user)
+      res.send(result)
+    })
+    
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
